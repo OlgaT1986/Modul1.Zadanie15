c18ca689037a40916ea88cb98b352d2483c27118
Ольга <evinn86@mail.ru>
2022-01-27 15:56:28 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie15/ISeries.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie15
+{
+    interface ISeries
+    {
+        // метод принимает аргументы - начальное значение и шаг
+        void setStart(int x); 
+        //метод возвращает следующее число
+        int getNext(); 
+        // метод сбрасывает
+        void reset(); 
+    }
+    // класс реализует интерфейс
+    class ArithProgression : ISeries 
+    {
+        // шаг
+        int step;  
+        // начальное значение
+        int startValue;  
+        // следующее значение
+        int currentValue;  
+        public int getNext()
+        {
+            // увеличение на шаг
+            currentValue += step; 
+            // возврат этого числа
+            return currentValue;  
+        }
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStart(int x)
+        {
+            // в начальное значение присваиваем х
+            startValue = x; 
+            currentValue = startValue;
+        }
+        // метод принимает s (это для шага)
+        public void setStep(int s) // метод принимает s
+        {
+            step = s; 
+        }
+
+    }
+    class GeomProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+
+}
+
---
+++ Modul1.Zadanie15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_Арифметическая прогрессия_");
+            ArithProgression arithProgression = new ArithProgression();
+            // установка начального значения
+            arithProgression.setStart(4);  
+            // установка шага
+            arithProgression.setStep(2); 
+            // вывод значения ариф.прогрессии
+            Console.WriteLine(arithProgression.getNext()); 
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            // сброс
+            arithProgression.reset(); 
+            Console.WriteLine("Сброс до начального значения-");
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine();
+            Console.WriteLine("_Геометрическая прогрессия_");
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(4);
+            geom.setStep(2);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset(); 
+            Console.WriteLine("Сброс до начального значения-");
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
+
